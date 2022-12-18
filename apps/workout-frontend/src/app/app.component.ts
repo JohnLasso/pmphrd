05bf01ebbe7c3a682fb70bff2acffd0097383a0e
Jacob Jäckel <jacob.jaeckel@hdi.de>
2022-12-18 16:00:14 +0100

    Initial commit

---
+++ apps/workout-frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'workout-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'workout-frontend';
+}
